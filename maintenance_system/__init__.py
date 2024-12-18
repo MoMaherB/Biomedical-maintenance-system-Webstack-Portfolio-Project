a79e6c8ac6c8ccb86d8ae23db36b3e0894721685
MoMaherB <maher1525@gmail.com>
2024-12-18 23:10:01 +0200

    - Make forms file. - Make User form. - Make register route. - Make register HTML page. - Add necessary imports.

--- maintenance_system/__init__.py
+++ maintenance_system/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///maintenance_system.db'
+
 db = SQLAlchemy(app)
 
 from maintenance_system import routes
--- maintenance_system/routes.py
+++ maintenance_system/routes.py
@@ -1,7 +1,23 @@
-from flask import render_template
-from maintenance_system import app
+from flask import render_template, redirect, url_for
+from maintenance_system import app, db
+from maintenance_system.models import User
+from maintenance_system.forms import UserForm
+
 
 
 @app.route("/")
 def home():
 	return render_template("home.html")
+
+
+#==================User Routes============================
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = UserForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for('users'))
+	return render_template("register.html", form=form)
