cba231fca7e094baadc9d8b237a13b72620da535
MoMaherB <maher1525@gmail.com>
2024-12-20 15:52:56 +0200

    - Add more user routes. - Add Department routs. - Make Add Department Form. - Add users, users, departments, department pages

--- maintenance_system/forms.py
+++ maintenance_system/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from maintenance_system.models import User
+from maintenance_system.models import User, Department
 
 #=========================================User Forms====================================================
 
@@ -21,3 +21,15 @@ class UserForm(FlaskForm):
 		user = User.query.filter_by(email=email.data.strip()).first()
 		if user:
 			raise ValidationError('This email already taken. Please choose another one.')
+		
+#=========================================Department Forms====================================================
+
+
+class DepartmentForm(FlaskForm):
+	name = StringField('Department Name', validators=[DataRequired(), Length(min=2, max=20)])
+	submit = SubmitField('Add')
+
+	def validate_name(self, name):
+		department = Department.query.filter_by(name=name.data.strip()).first()
+		if department:
+			raise ValidationError('This department name already exists. Please choose another one.')
--- maintenance_system/routes.py
+++ maintenance_system/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, request
 from maintenance_system import app, db
-from maintenance_system.models import User
-from maintenance_system.forms import UserForm
+from maintenance_system.models import User , Department
+from maintenance_system.forms import UserForm, DepartmentForm
 
 
 
@@ -12,12 +12,48 @@ def home():
 
 #==================User Routes============================
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
 	form = UserForm()
 	if form.validate_on_submit():
 		user = User(username=form.username.data, email=form.email.data, password=form.password.data)
 		db.session.add(user)
 		db.session.commit()
+		flash('User has been created successfully!', 'success')
 		return redirect(url_for('users'))
-	return render_template("register.html", form=form)
+	return render_template('register.html', form=form)
+
+@app.route('/users')
+def users():
+	users = User.query.all()
+	return render_template('users.html', users=users)
+
+@app.route('/users/<int:id>')
+def user(id):
+	user = User.query.get_or_404(id)
+	return render_template('user.html', user=user)
+
+#=================Department Routes==========================
+
+@app.route('/departments')
+def departments():
+	departments = Department.query.all()
+	return render_template('departments.html', departments=departments)
+
+@app.route('/departments/<int:id>')
+def department(id):
+	department = Department.query.get_or_404(id)
+
+	render_template('department.html',department=department )
+
+@app.route('/add_department', methods=['GET', 'POST'])
+def add_department():
+	departments = Department.query.all()
+	form = DepartmentForm()
+	if form.validate_on_submit():
+		new_department = Department(name=form.name.data)
+		db.session.add(new_department)
+		db.session.commit()
+		return redirect(url_for('departments'))
+	
+	return render_template('departments.html', form=form, departments=departments)
