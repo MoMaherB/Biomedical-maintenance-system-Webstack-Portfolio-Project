d2977796ec745a77807882f82d0fb718b3f9c420
MoMaherB <maher1525@gmail.com>
2024-12-21 00:38:50 +0200

    - Change add, update and delete Departemnt approch. - use modals from bootstrap to make the application more modern and dynamic. - Edit Tempaltes to match the changes.

--- maintenance_system/routes.py
+++ maintenance_system/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request
 from maintenance_system import app, db
 from maintenance_system.models import User , Department, Device
-from maintenance_system.forms import UserForm, DepartmentForm, UpdateDepartmentForm, DeviceForm
+from maintenance_system.forms import UserForm, DepartmentForm, DeviceForm
 
 
 
@@ -37,48 +37,55 @@ def user(id):
 #====================================Department Routes======================================
 
 
-@app.route('/departments/')
+@app.route('/departments/', methods=['GET', 'POST'])
 def departments():
+	form = DepartmentForm()
 	departments = Department.query.all()
-	return render_template('departments.html', departments=departments)
+	return render_template('departments.html', departments=departments, form=form)
 
 @app.route('/departments/<int:id>/')
 def department(id):
 	department = Department.query.get_or_404(id)
-
 	return render_template('department.html',department=department )
 
 @app.route('/add_department', methods=['GET', 'POST'])
 def add_department():
-	departments = Department.query.all()
 	form = DepartmentForm()
-	if form.validate_on_submit():
+	if request.method == 'GET':
+		return redirect(url_for('departments'))
+	elif form.validate_on_submit():
 		new_department = Department(name=form.name.data)
 		db.session.add(new_department)
 		db.session.commit()
+		flash(f'Department {form.name.data} has been created successfully!', 'success')
 		return redirect(url_for('departments'))
 	
+	departments = Department.query.all()
+	flash('Department name already exists. Please choose another one.', 'danger')
 	return render_template('departments.html', form=form, departments=departments)
 
-@app.route('/delete_department/<int:id>')
+@app.route('/delete_department/<int:id>', methods=['POST'])
 def delete_department(id):
 	department = Department.query.get_or_404(id)
 	db.session.delete(department)
 	db.session.commit()
+	flash(f'Department {department.name} has been deleted successfully!', 'success')
 	return redirect(url_for('departments'))
 
 @app.route('/update_department/<int:id>', methods=['GET', 'POST'])
 def update_department(id):
-	departments = Department.query.all()
 	department = Department.query.get_or_404(id)
-	form = UpdateDepartmentForm()
-	if form.validate_on_submit():
+	form = DepartmentForm()
+	if request.method == 'GET':
+		return redirect(url_for('departments'))
+	elif form.validate_on_submit():
 		department.name = form.name.data
 		db.session.commit()
+		flash('Department has been updated successfully!', 'success')
 		return redirect(url_for('departments'))
-	elif request.method == 'GET':
-		form.name.data = department.name
-	return render_template('departments.html', form=form, departments=departments)
+	else:
+		flash('Department name already exists. Please choose another one.', 'danger')
+		return redirect(url_for('departments'))
 
 #====================================Devices Routes======================================
 
@@ -99,6 +106,20 @@ def add_device(department_id):
 		db.session.commit()
 		return redirect(url_for('devices', department_id=department_id))
 	return render_template('devices.html', form=form, department=department)
+
+@app.route('/devices/<int:device_id>')
+def device(device_id):
+	device = Device.query.get_or_404(device_id)
+	return render_template('device.html', device=device)
+
+@app.route('/delete_device/<int:device_id>')
+def delete_device(device_id):
+	device = Device.query.get_or_404(device_id)
+	db.session.delete(device)
+	db.session.commit()
+	return redirect(url_for('devices', department_id=device.department_id))
+
+
 
 
 
